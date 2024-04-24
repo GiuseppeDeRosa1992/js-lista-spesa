13f7e5fb764469f7e8ee153c83b75f8ffa67d4aa
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-24 15:31:00 +0200

    M1- added array with variable shoppingList and console.log and added cycle for with console.log shoppingList

---
+++ js/main.js
@@ -0,0 +1,21 @@
+console.log("Ciao Giuseppe");
+
+//CREO LISTA DELLA SPESA CON ARRAY
+const shoppingList = [
+    "Uova",
+    "Latte",
+    "Burro",
+    "Cioccolata",
+    "Salame",
+    "Acqua",
+    "Coca",
+    "Pasta",
+    "Banane",
+    "Pesce",
+]
+
+console.log(shoppingList)
+
+for (let i = 0; i < shoppingList.length; i++ ) {
+    console.log(i, shoppingList[i])
+}
