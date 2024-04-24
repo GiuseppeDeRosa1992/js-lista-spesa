4e2d4cc050a41fdee2f4acb10fc121709a4ef221
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-24 15:44:34 +0200

    M-2 added cycle while with console.log shoppingList and reset value i when cycle while is finish

--- js/main.js
+++ js/main.js
@@ -12,10 +12,26 @@ const shoppingList = [
     "Pasta",
     "Banane",
     "Pesce",
-]
+];
 
 console.log(shoppingList)
 
 for (let i = 0; i < shoppingList.length; i++ ) {
-    console.log(i, shoppingList[i])
+    console.log(i, shoppingList[i]);
 }
+
+//Do alla variabile i il valore 0 che poi userò nel ciclo while e la rendo visibile in console per vedere che valore ha
+let i = 0;
+console.log(i);
+//inizio il ciclo while
+while (i < shoppingList.length) {
+    //Console.log del valore attuale della i che corrisponde anche al valore della cosa da comprare
+    console.log(i, shoppingList[i])
+    //aumento la i di 1 per far ripartire il ciclo while finchè non mi ha stampato tutti gli elementi della lista della spesa
+    i++
+}
+//controllo che valore ha il contatore i una volta finito il ciclo while
+console.log(i)
+//Resetto il valore del contatore i a 0 perchè dopo il ciclo while mi dava valore 10 e può crearmi problemi in futuro
+i = 0
+console.log(i)
